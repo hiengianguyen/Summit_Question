5572e33b3e11b47a2dc6ed34d2df59def2621ad9
hiengianguyen <hiennguyen2060@gmail.com>
2024-08-30 22:51:20 +0700

    refactor to ES6

--- public/js/getData.js
+++ public/js/getData.js
@@ -1,16 +1,37 @@
-var myApi = "https://66ccad7da4dd3c8a71b87616.mockapi.io/textContents";
-var content = document.querySelector(".text-content");
+const myApi = "https://66ccad7da4dd3c8a71b87616.mockapi.io/textContents";
+const content = document.querySelector(".text-content");
+const tittleElement = document.querySelector("title");
+const titleOriginal = tittleElement.innerHTML;
+const alertH = document.querySelector(".alert");
+const alertFull = document.querySelector(".alert");
+const tiClose = document.querySelector(".alert-close");
+const alertContainer = document.querySelector(".alert-container");
+const contentAlert = document.querySelector(".content-alert p");
 
-fetch(myApi)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    var textContents = data.map(function (text) {
-      return `
+try {
+  fetch(myApi)
+    .then((response) => response.json())
+    .then((data) => {
+      const textContents = data.map(
+        (text) =>
+          `
       <div>${text.textcontent}</div>
-      `;
-    });
+      `
+      );
 
-    content.innerHTML = textContents.join("");
+      content.innerHTML = textContents.join("");
+    });
+} catch (error) {
+  // hiện alert
+  alertH.style.display = "flex";
+  // nếu lọt vào catch thì thay text thành:
+  contentAlert.innerHTML = "Lỗi server rùi =((";
+  // nếu lỗi server thì thay tittle thành:
+  tittleElement.innerHTML = "Lỗi Server";
+  // close
+  tiClose.addEventListener("click", () => {
+    alertH.style.display = "none";
+    // khôi phục tittle lại ban đầu
+    tittleElement.innerHTML = titleOriginal;
   });
+}
--- public/js/selectionClass.js
+++ public/js/selectionClass.js
@@ -1,21 +1,20 @@
-var postApi = "https://66ccad7da4dd3c8a71b87616.mockapi.io/classlists";
-var selectClass = document.querySelector(".select-class");
+const postApi = "https://66ccad7da4dd3c8a71b87616.mockapi.io/classlists";
+const selectClass = document.querySelector(".select-class");
 
 // get Data
 fetch(postApi)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    var classLists = data.map(function (classs) {
-      return `
-       <option value="${
-         classs.class
-       }" ${classs.class == "10a4" ? "selected" : ""} >
+  .then((response) => response.json())
+  .then((data) => {
+    const classLists = data.map(
+      (classs) =>
+        `
+       <option value="${classs.class}" ${
+          classs.class == "10a4" ? "selected" : ""
+        } >
        ${classs.class}
        </option>
-       `;
-    });
+       `
+    );
 
     selectClass.innerHTML = classLists.join();
   });
