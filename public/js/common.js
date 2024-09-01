4c055d4db285b1700a31fccdc27c07351789a51a
hiengianguyen <hiennguyen2060@gmail.com>
2024-09-01 17:44:15 +0700

    refactor header text content

--- public/js/common.js
+++ public/js/common.js
@@ -3,7 +3,7 @@ const boyAvatars = [
   "https://firebasestorage.googleapis.com/v0/b/submit-question.appspot.com/o/avatars%2Fboy-2.png?alt=media&token=6986f34c-1e54-493a-910f-ae8b42a71974",
   "https://firebasestorage.googleapis.com/v0/b/submit-question.appspot.com/o/avatars%2Fboy-3.png?alt=media&token=585e03ae-0538-463f-a034-7139be777f56",
   "https://firebasestorage.googleapis.com/v0/b/submit-question.appspot.com/o/avatars%2Fboy-4.png?alt=media&token=823efd1b-5b58-41b2-8990-5cff1342bef1",
-  "https://firebasestorage.googleapis.com/v0/b/submit-question.appspot.com/o/avatars%2Fboy-5.png?alt=media&token=d28847ae-d624-4bc0-8275-32d5737eda68",
+  "https://firebasestorage.googleapis.com/v0/b/submit-question.appspot.com/o/avatars%2Fboy-5.png?alt=media&token=d28847ae-d624-4bc0-8275-32d5737eda68"
 ];
 
 const girlAvatars = [
@@ -11,7 +11,7 @@ const girlAvatars = [
   "https://firebasestorage.googleapis.com/v0/b/submit-question.appspot.com/o/avatars%2Fgirl-2.png?alt=media&token=ca06901b-38cd-40d6-aa42-33f19bd5b5d0",
   "https://firebasestorage.googleapis.com/v0/b/submit-question.appspot.com/o/avatars%2Fgirl-3.png?alt=media&token=dc2ef1f0-2e87-4919-8209-457b677fc546",
   "https://firebasestorage.googleapis.com/v0/b/submit-question.appspot.com/o/avatars%2Fgirl-4.png?alt=media&token=f18b0cff-1813-40d0-8aac-6b9dee1aaee7",
-  "https://firebasestorage.googleapis.com/v0/b/submit-question.appspot.com/o/avatars%2Fgirl-5.png?alt=media&token=cf9957c9-7b45-4645-b716-a1e2da44dbbb",
+  "https://firebasestorage.googleapis.com/v0/b/submit-question.appspot.com/o/avatars%2Fgirl-5.png?alt=media&token=cf9957c9-7b45-4645-b716-a1e2da44dbbb"
 ];
 
 const getFormattedCurrentDateTime = () => {
